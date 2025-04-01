831e9742989a6ac6a41ed6abef4dcd93695187f8
Amarildo <amarildokola36@gmail.com>
2025-04-01 20:23:09 +0200

    test deployment

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -17,7 +17,7 @@ export default function UserForm({
   // Simple validation functions
   const isFullNameValid = (value) => {
     const words = value.trim().split(/\s+/);
-    return value.length >= 5 && words.length >= 2;
+    return value.length >= 3 && words.length >= 2;
   }; // Full Name Validation: At least 5 characters & 2 words
   const isPhoneNumberValid = (value) => /^[0-9]*$/.test(value); // Only numbers
   const isEmailValid = (value) => /\S+@\S+\.\S+/.test(value); // Simple email regex
@@ -36,7 +36,7 @@ export default function UserForm({
         />
         {!isFullNameValid(fullName) && fullName !== "" && (
           <span className="text-red-500 text-xs">
-            Full Name must be at least 5 characters and at least 2 words.
+            Full Name must be at least 3 characters and at least 2 words.
           </span>
         )}
       </div>
